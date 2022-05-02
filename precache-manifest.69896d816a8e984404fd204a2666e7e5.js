e7e8332f69cd7bba5d964fa138293788a29e6f75
johnabednego <jajilima@st.ug.edu.gh>
2022-05-02 03:52:01 +0000

    Updates

---
+++ precache-manifest.69896d816a8e984404fd204a2666e7e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e16fbc41b7e7b714cef",
+    "url": "/mobile-weather/static/css/main.fca9bb8b.chunk.css"
+  },
+  {
+    "revision": "7e16fbc41b7e7b714cef",
+    "url": "/mobile-weather/static/js/main.7e16fbc4.chunk.js"
+  },
+  {
+    "revision": "6b128423ec6069414a74",
+    "url": "/mobile-weather/static/css/1.b96fd7b8.chunk.css"
+  },
+  {
+    "revision": "6b128423ec6069414a74",
+    "url": "/mobile-weather/static/js/1.6b128423.chunk.js"
+  },
+  {
+    "revision": "323357fa95c28baf4772",
+    "url": "/mobile-weather/static/js/runtime~main.323357fa.js"
+  },
+  {
+    "revision": "aecdd80c4241821b52ed09131af6965c",
+    "url": "/mobile-weather/index.html"
+  }
+];
