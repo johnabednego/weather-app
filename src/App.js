fde2a609cd987d63614503284e1c4b2a81f0791c
johnabednego <jajilima@st.ug.edu.gh>
2022-05-24 02:08:51 +0000

    error

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ class App extends Component {
       })
       .catch((error) => {
         this.setState({
-          error: "ERROR OCCURRED: Check internet connection",
+          error: "ERROR: "+error,
         });
       });
   };
